6eba3f6e31cd082fb83f4d43050f34ff7a3eb1bc
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-28 15:33:27 +0200

    remove dependencies

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/pluralsight/gomodules
 
 go 1.15
-
-require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	rtr := mux.NewRouter()
+	//rtr := mux.NewRouter()
 
-	rtr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		w.Write([]byte("Topic: " + vars["topic"]))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//vars := mux.Vars(r)
+		//w.Write([]byte("Topic: " + vars["topic"]))
+		w.Write([]byte("Hello World!"))
 	})
-	http.Handle("/", rtr)
+	//http.Handle("/", rtr)
 
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
