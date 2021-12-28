a259da8a8853c68f517baf43090703de733a9039
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-28 16:46:04 +0200

    multiple versions

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/pluralsight/gomodules
 
 go 1.15
+
+require (
+	rsc.io/quote v1.5.2 // indirect
+	rsc.io/quote/v2 v2.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"rsc.io/quote"
+	quotev2 "rsc.io/quote/v2"
 )
 
 func main() {
-	//rtr := mux.NewRouter()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//vars := mux.Vars(r)
-		//w.Write([]byte("Topic: " + vars["topic"]))
-		w.Write([]byte("Hello World!"))
-	})
-	//http.Handle("/", rtr)
-
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(quote.Hello())
+	fmt.Println(quotev2.HelloV2())
 }
